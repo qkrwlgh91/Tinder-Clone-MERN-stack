c207126764634a8f6a89d36fb8d2bb35ca4c6882
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-10-24 22:52:30 +0900

    added header styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     // BEM class naming convention
     <div className="app">
-      <h1>Lets build the MERN TINDER clone guys!!!</h1>
+
+    {/* Header */}
+    <Header />
+    {/* TinderCards */}
+
+    {/* SwipeButtons */}
+
     </div>
   );
 }
